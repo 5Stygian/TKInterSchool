d77853507b04aaa925e58302211b30aaf17ca1ff
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-08 15:46:18 -0700

    Update main.py

--- gameshow/main.py
+++ gameshow/main.py
@@ -1,4 +1,13 @@
 from tkinter import *
+from PIL import Image, ImageTk
+
+score = 0
+
+image1 = open("/latex/q1.png")
+image2 = open("/latex/q2.png")
+image3 = open("/latex/q3.png")
+image4 = open("/latex/q4.png")
+image5 = open("/latex/q5.png")
 
 win = Tk()
 
@@ -6,78 +15,52 @@ win.config(bg="cornflower blue")
 win.geometry("900x600")
 win.title("PyGame is a better lib")
 
-class MyButton:
-    """
-    Args:
-        text(str): text displayed on the button
-        xPos(int): x position of the button on the TKinter window
-        yPos(int): y position of the button on the TKinter window
-        xDim(int): length of the button
-        yDim(int): width of the button
-        bg(str): background color of the button
-        fg(str): text color of the button
-        setpos(int): sets the button's position to either the left or the right
-        correct(bool): set whether the button is correct or not
-    """
-    def __init__(self, text=None, xPos=None, yPos=None, xDim=None, yDim=None, bg=None, fg=None, setpos=None, correct=None):
-        self.text    = text
-        self.xPos    = xPos
-        self.yPos    = yPos
-        self.xDim    = xDim
-        self.yDim    = yDim
-        self.bg      = bg
-        self.fg      = fg
-        self.setpos  = setpos
-        self.correct = correct
-    
-    def setposFoo(self):
-        if self.setpos == 0:
-            self.xPos -= 100
-        elif self.setpos == 1:
-            self.xPos += 100
-        else:
-            pass
+def main():
+    user_input = entry.get().lower()
+    user_input = user_input.strip()
 
-    def foo(self):
-        if self.correct == True:
-            win.config(bg="black")
-        elif self.correct == False:
-            win.config(bg="white")
-        else:
-            pass
 
-myButton1 = MyButton("button1", 400, 300, 10, 3, "wheat", "red", 0, False)
-myButton1.setposFoo()
+    entry.delete(0, END)  # Clear the entry after retrieving input
 
-button1 = Button(
-    win,
-    text                = myButton1.text,
-    highlightbackground = myButton1.bg,
-    fg                  = myButton1.fg,
-    width               = myButton1.xDim,
-    height              = myButton1.yDim,
-    command             = myButton1.foo
+entry = Entry(
+    win, 
+    width = 30
 )
-button1.place(
-    x = myButton1.xPos,
-    y = myButton1.yPos
+entry.place(
+    x = 325,
+    y = 250
 )
 
-myButton2 = MyButton("button2", 400, 300, 10, 3, "wheat", "red", 1, True)
-myButton2.setposFoo()
-
-button2 = Button(
-    win,
-    text                = myButton2.text,
-    highlightbackground = myButton2.bg,
-    fg                  = myButton2.fg,
-    width               = myButton2.xDim,
-    height              = myButton2.yDim,
-    command             = myButton2.foo
+label = Label(
+    text = "Question 1",
+    fg   = "black",
+    bg   = "white",
+    font = ("Arial", 24, "bold")
 )
-button2.place(
-    x = myButton2.xPos,
-    y = myButton2.yPos
+label.place(
+    x = 50,
+    y = 20
+)
+
+scoreLabel = Label(
+    text = f"Score: {score}",
+    fg   = "black",
+    bg   = "white",
+    font = ("Arial", 24, "bold")
+)
+scoreLabel.place(
+    x = 780,
+    y = 20
+)
+
+button = Button(
+    win, 
+    text    = "Submit", 
+    command = main
+)
+button.place(
+    x = 400,
+    y = 300
 )
 
 win.mainloop()
